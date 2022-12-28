05418220ba7c83f708c21c376b54217adc0cdc96
Diego Alcoba <diegoalcobaarias@gmail.com>
2022-12-28 19:30:26 +0100

    Cambios y declaracions globales

--- LuZECita.c
+++ LuZECita.c
@@ -2,38 +2,57 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/syscall.h>
 #include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <time.h>
+#include <linux/unistd.h>
 #include <errno.h>
-#define NUM_TECNICOS 2
-#define NUM_RESPONSABLE_REPARACIONES 2
-#define NUM_ENCARGADO 1
 
-void *hilo_creado(void *arg){
-	printf("El hilo se ha creado correctamente\n");
-	pthread_exit(NULL);
-}
+#define NTECNICOS 2.0
+#define NRESPONSABLES 2.0
+#define NENCARGADOS 1.0
 
+/*Variables globales*/
+pthread_mutex_t semaforoFichero;
+pthread_mutex_t semaforoColaClientes;
+pthread_mutex_t semaforoSolicitudes;
+
+pthread_t *arrayHilosClientes; //Array de hilo-cliente
+
+int nClientesApp; //Contador clientes app
+int nClientesRed; //Contador clientes red
+int nSolicitudesDomiciliarias; //N solicitudes domiciliarias (4 para que el responsable viaje)
+
+/*Arrays dinámicos que almacenan a los clientes (estructura)*/
+struct cliente *arrayClientes;
+struct cliente *arrayClientesSolicitudes; //Clientes a los que tiene que visitar el responsable
+
+/*Estructuras*/
+struct cliente {
+	char *id;
+	int atendido;
+	char *tipo;
+	int prioridad;
+	int solicitud; //Quizás mejor un booleano? Solicitud sí/no
+};
+
+struct trabajador { 
+	char *id;
+	int clientesAtendidos;
+	char *tipo; //Tecnico, responsable o encargado
+};
+
+/*Fichero de log*/
+FILE *logFile;
 
 int main(int argc, char* argv[]) {
-	pthread_t tecnico, responsable_reparaciones, encargado, clientes_app, clientes_red;
-	int resultado_tecnico= pthread_create(&tecnico, NULL, hilo_creado, NULL);
-	if(resultado_tecnico!=0){
-		//ha ocurrido un error al crear el hilo
-		perror("Error al crear el hilo tecnico\n");
-		return 1;
-	}
-	int resultado_responsable_reparaciones= pthread_create(&responsable_reparaciones, NULL, hilo_creado, NULL);
-	if(resultado_responsable_reparaciones!=0){
-		//ha ocurrido un error al crear el hilo
-		perror("Error al crear el hilo responsable de reparaciones\n");
-		return 1;
-	}
-	int resultado_encargado= pthread_create(&encargado, NULL, hilo_creado, NULL);
-	if(resultado_encargado!=0){
-		//ha ocurrido un error al crear el hilo
-		perror("Error al crear el hilo encargado\n");
-		return 1;
-	}
+
+
+
 	return 0;
 }
 
