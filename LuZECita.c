689c80b61b4a59839bc4bdd19f6f4fc482f6ed80
alonsogonzalo <galono00@estudiantes.unileon.es>
2022-12-29 11:52:35 +0100

    Parte de los tecnicos casi acabada con algun fallo en si estan libre o no

--- LuZECita.c
+++ LuZECita.c
@@ -39,8 +39,8 @@ struct cliente *arrayClientesSolicitudes; //Clientes a los que tiene que visitar
 /*Estructuras*/
 //estructura que guarda la informacion del cliente
 typedef struct {
-	char *id;		//no seria mejor poner el id como int?
-	int atendido;
+	char *id;		//no seria mejor poner el id como int? NO PORQUE EL IDENTIFICADOR ES CLIAPP_1
+	int atendido; //0 si no esta atendido 1 si si 
 	char *tipo;
 	int prioridad;
 	bool solicitud;
@@ -50,27 +50,87 @@ typedef struct {
 typedef struct { 
 	char *id;
 	int clientesAtendidos;
-	char *tipo; //Tecnico, responsable o encargado
+	char *tipo; //Tecnico, responsable o encargado T R oE
+	int libre; //0 si esta libre 1 si esta ocupado
 }Trabajador;
 
 /*Fichero de log*/
 FILE *logFile;
+
+//funcion para crear un tecnico
+Trabajador generar_Tecnico(){
+	//Tecnico 1
+		static int id1=0;
+		Trabajador tecnico_1;
+		tecnico_1.id=++id1;
+		tecnico_1.clientesAtendidos=0;
+		tecnico_1.tipo=T;
+		tecnico_1.libre=0;
+	//Tecnico 2
+		static int id2=0;
+		Trabajador tecnico_2;
+		tecnico_2.id=++id2;	
+		tecnico_2.clientesAtendidos=0;
+		tecnico_2.tipo=T;
+		tecnico_2.libre=0;
+}
 
 //funcion para generar un cliente
 Cliente generar_cliente(){
 	static int id = 0;
 	Cliente c;
 	c.id = ++id;
-	c.atendido = false;
+	c.atendido = 0;//0 si no esta atendido 1 sis si
 
 	return c;
 }
+/*funcion del encargado es decir atender a los clientes con problemas en la app*/
+void atender_cliente_app(){
+	//IMPORTANTE:::::AÑADIR LO DE LA PRIORIDAD
+	//Se comprueba si el tenico esta libre
+	if(tecnico_1.libre==0){
+		printf("Se va a atender a un cliente con problemas en la app");
+		printf("Bienvenido cliente le esta atendiendo el tecnico_1");
+		tecnico1.libre=1;
+		//Compruebo que el cliente que atiendo sea de app
+		if(cliente.tipo==a){
+			printf("El cliente se ha atendido");
+			//Cambio su estado a atendido
+			cliente.atendido=1;
+			++tecnico_1.clientesAtendidos;
+			//Si hay 5 descanso 5 seg
+			if(tecnico_1.clientesAtendidos==5){
+				tecnico_1.libre=1;
+				sleep(5);
+			}
+		
+		}
+	}else if (tecnico_2.libre==0){
+		printf("Se va a atender a un cliente con problemas en la app");
+		printf("Bienvenido cliente le esta atendiendo el tecnico_2");
+		tecnico2.libre=1;
+		//Compruebo que el cliente que atiendo sea de app
+		if(cliente.tipo==a){
+			printf("El cliente se ha atendido");
+			//Cambio su estado a atendido
+			cliente.atendido=1;
+			++tecnico_2.clientesAtendidos;
+			//Si hay 5 descanso 5 seg
+			if(tecnico_2.clientesAtendidos==5){
+				tecnico_2.libre=1;
+				sleep(5);
+			}
+		
+		}
+	}else{}
 
+//IMPORTANTE::::::AÑADIR CONDICION DE QUE SI ESTAN LOS DOS OCUPADOS ATENDERLOS ENCARGADO
+}
 //funcion para atender a un cliente
 void atender_cliente(){
 	printf("Se esta atendiendo al cliente %d (%c)\n, ");
 	sleep(1);	//se simula el tiempo de atencion
-}
+	}
 
 int main(int argc, char* argv[]) {
 
